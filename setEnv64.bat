ae0af531865f8f914ad6b97fdc1ef46e899cae45
donghaiwang <whd@hutb.edu.cn>
2025-07-07 12:37:20 +0800

    fix

--- setEnv64.bat
+++ setEnv64.bat
@@ -23,9 +23,10 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 
 :: python 
 :: C:\software\anaconda3\envs\carla_dev
-call cmd.exe \k "C:\software\anaconda3\Scripts\activate.bat carla_dev && python --version"
+call C:\software\anaconda3\Scripts\activate.bat carla_dev && python --version
 :: conda activate carla_dev
-:: call python --version
+echo Python version after activate carla_dev:
+call python --version
 :: call exit /b
 
 
