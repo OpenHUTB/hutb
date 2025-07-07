2cccff2aaef1fe69ab0ca8ac99136d5abd341755
donghaiwang <whd@hutb.edu.cn>
2025-07-07 13:04:08 +0800

    fix

--- Util/BuildTools/Check.bat
+++ Util/BuildTools/Check.bat
@@ -138,8 +138,8 @@ if %PYTHON_API%==true (
     echo Running Python API for Python ${PY_VERSION} unit tests.
     :: TODO pip install nose2 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
     echo Current directory: %cd%
-    echo Test command: %python_path% -m nose2 %EXTRA_ARGS%
-    %python_path% -m nose2 %EXTRA_ARGS%
+    echo Test command: %python_path% -m nose2
+    %python_path% -m nose2
 
     if %XML_OUTPUT%==true (
         move test-results.xml ${CARLA_TEST_RESULTS_FOLDER}/python-api-3.xml
