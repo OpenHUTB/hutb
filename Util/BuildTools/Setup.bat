f3505618cf019483a0c91cc0e94b654883e378d0
donghaiwang <whd@hutb.edu.cn>
2025-07-31 16:22:33 +0800

    downgrade boost to 1.84.0

--- Util/BuildTools/Setup.bat
+++ Util/BuildTools/Setup.bat
@@ -25,7 +25,7 @@ rem -- Parse arguments ---------------------------------------------------------
 rem ============================================================================
 
 set GENERATOR=""
-set BOOST_VERSION=1.86.0
+set BOOST_VERSION=%BOOST_VERSION%
 set INSTALLERS_DIR=%ROOT_PATH:/=\%Util\InstallersWin\
 set VERSION_FILE=%ROOT_PATH:/=\%Util\ContentVersions.txt
 set Carla_ROOT_DIR=%ROOT_PATH:/=\%\
