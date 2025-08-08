6961f3b737128aebaebc07ebd8f2a1f98a564b39
donghaiwang <whd@hutb.edu.cn>
2025-08-08 11:23:32 +0800

    [carla] Adding Cosmos Control Visualization Sensor (#9122)
    * Uploading empty Sensor class for testing
    
    * Standard functions added to the sensor
    
    * Fixes to the sensor declaration
    
    * Moved DrawDebug logic to the sensor
    
    Persistent Lines are not getting flushed after the sensor is no longer in use
    
    * Reverted Changes to game mode base
    
    * Fixed Skeletal Meshes, persistent lines not visible in server
    
    * Fix CosmosControlSensor inlcludes for Linux
    
    * Add RoadSpline Actor
    
    * Spawn example RoadSpline
    
    * Spawn a RoadSpline every lane
    
    * Add new boundary types based on LaneType
    
    * Avoid spawning splines in junctions, and pass BoundaryType to RoadSpline
    
    * Spawn lane borders
    
    * Add junction flag to RoadSpline, clean up spawned actor names
    
    * Integration of RoadSpline class in the visualization
    
    * Crosswalks now render as Solid Boxes
    
    * Fix gaps between roadsplines
    
    * Adding Lines for Junction and Fixing Rendering in Build
    
    * Fixed Rendering of Poles, rendering with transparency, not rendering ego vehicle, derived LineBatch class
    
    * New Material Added, Unlit Viewmode removed for material to take effect
    
    * Added a Cosmos Visualization PostProcessing Material
    
    * Creating Opaque Material for Solid Box rendering, code cleanup
    
    * Fixing material loading to work with build
    
    * Changes for the PR
    
    * EOF added in LineBatchComp
    
    * Updated Copyright Notices
    
    * Exposed Road Spline SampleDistance
    
    * Update test_sensor_tick_time.py to avoid testing cosmos
    
    ---------
    
    Co-authored-by: Aperiss <perissanchezantonio@gmail.com>
    
    Added a new type of CosmosControlSensor, sensor with the requirements for a Control Signal for the Cosmos environment.
    The sensor can be spawned with the cosmos_visualization definition in python.
    
    Added a ULineBatchComponent derived class that renders exclusively on the CosmosControlSensor.
    Added a new material for the sensor, which uses an exponential falloff by @Aperiss
    @Aperiss Added a new class to extract xodr files road data and make it available in the GameMode.

--- LibCarla/source/carla/sensor/SensorRegistry.h
+++ LibCarla/source/carla/sensor/SensorRegistry.h
@@ -49,6 +49,7 @@ class ARayCastLidar;
 class ASceneCaptureCamera;
 class ASemanticSegmentationCamera;
 class AInstanceSegmentationCamera;
+class ACosmosControlSensor;
 class ARssSensor;
 class FWorldObserver;
 class ADReyeVRSensor; // DReyeVR forward declaration
@@ -85,6 +86,7 @@ namespace sensor {
     std::pair<ASceneCaptureCamera *, s11n::ImageSerializer>,
     std::pair<ASemanticSegmentationCamera *, s11n::ImageSerializer>,
     std::pair<AInstanceSegmentationCamera *, s11n::ImageSerializer>,
+    std::pair<ACosmosControlSensor *, s11n::ImageSerializer>,
     std::pair<FWorldObserver *, s11n::EpisodeStateSerializer>,
     std::pair<ADReyeVRSensor *, s11n::DReyeVRSerializer>,
     std::pair<AFisheyeSensor *, s11n::ImageSerializerCube>,
@@ -120,6 +122,7 @@ namespace sensor {
 #include "Carla/Sensor/SceneCaptureCamera.h"
 #include "Carla/Sensor/SemanticSegmentationCamera.h"
 #include "Carla/Sensor/InstanceSegmentationCamera.h"
+#include "Carla/Sensor/CosmosControlSensor.h"
 #include "Carla/Sensor/WorldObserver.h"
 #include "Carla/Sensor/DReyeVRSensor.h" // DReyeVRSensor header file
 #include "Carla/Sensor/FisheyeSensor.h"
