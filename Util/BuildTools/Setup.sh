fccd2fe6cb6b9f7452014621de925cd863b9523f
jiandaoshou-aidehua <2893666867@qq.com>
2026-01-30 22:49:15 +0800

    设置 -stdlib=libc++ 编译libxerces

--- Util/BuildTools/Setup.sh
+++ Util/BuildTools/Setup.sh
@@ -538,7 +538,7 @@ else
   pushd ${XERCESC_SRC_DIR}/build >/dev/null
 
   cmake -G "Ninja" \
-      -DCMAKE_CXX_FLAGS="-std=c++14 -fPIC -w -I${LLVM_INCLUDE} -L${LLVM_LIBPATH}" \
+      -DCMAKE_CXX_FLAGS="-std=c++14 -stdlib=libc++ -fPIC -w -I${LLVM_INCLUDE} -L${LLVM_LIBPATH}" \
       -DCMAKE_INSTALL_PREFIX="../../${XERCESC_INSTALL_SERVER_DIR}" \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=OFF \
