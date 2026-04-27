6870c47e5dbb8e760f4a5d0ec4cb81ec6ec74b64
donghaiwang <whd@hutb.edu.cn>
2026-04-27 10:31:16 +0800

    手动更新资产

--- Util/BuildTools/Check.bat
+++ Util/BuildTools/Check.bat
@@ -12,21 +12,21 @@ rem Bat script that compiles and exports the carla project (carla.org)
 rem Run it through a cmd with the x64 Visual C++ Toolset enabled.
 rem https://wiki.unrealengine.com/How_to_package_your_game_with_commands
 
-:: 只表示将要“运行的”bat命令的folder，不包含bat名称自己。
-:: 注意，不是“运行处”的folder （该功能用%cd%实现）
-:: %0-%9代表的是batch文件的参数。%1-%9 是batch名称之后的命令行参数，%0代表batch文件自己。
-:: d表示盘符，p表示不带盘符的路径，那么dp就表示带盘符的路径
+rem 只表示将要“运行的”bat命令的folder，不包含bat名称自己。
+rem 注意，不是“运行处”的folder （该功能用%cd%实现）
+rem %0-%9代表的是batch文件的参数。%1-%9 是batch名称之后的命令行参数，%0代表batch文件自己。
+rem d表示盘符，p表示不带盘符的路径，那么dp就表示带盘符的路径
 set LOCAL_PATH=%~dp0
-:: 当前运行脚本的文件名
-:: -[Check]
+rem 当前运行脚本的文件名
+rem -[Check]
 set FILE_N=-[%~n0]:
 
-:: 打印批处理脚本的参数（为了调试）
+rem 打印批处理脚本的参数（为了调试）
 rem Print batch params (debug purpose)
 :: -[Check]: [Batch params]:
 echo %FILE_N% [Batch params]: %*
 
-:: 衡量测试的总共时间
+rem 衡量测试的总共时间
 rem Measure overall execution time of testing
 call :get_current_time_in_seconds T_START_OVERALL
 
@@ -145,7 +145,7 @@ rem The directory of CarlaUE4.exe
 set BUILD_FOLDER=%INSTALLATION_DIR%UE4Carla/%CARLA_VERSION%/
 :: debug only (rename with no dirty)
 if %IS_DEBUG%==true (
-    set BUILD_FOLDER=D:\hutb\Build\UE4Carla\debug\
+    set BUILD_FOLDER=%INSTALLATION_DIR%UE4Carla\debug\
 )
 
 set exe_dir=%BUILD_FOLDER:\=/%WindowsNoEditor/%
