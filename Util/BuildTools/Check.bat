9842eb57317eb8bfe4c37d7ee41740a4ddda4b8d
donghaiwang <whd@hutb.edu.cn>
2026-05-01 09:34:38 +0800

    修复进行air示例测试时连不上测试端口的问题

--- Util/BuildTools/Check.bat
+++ Util/BuildTools/Check.bat
@@ -207,10 +207,10 @@ if %AIR_TESTS%==true (
         call conda activate hutb_3.%%i
         cd %ROOT_PATH:/=\%PythonAPI\examples\air\
         rem python 3.7 - 3.11 is normal, but 3.12, 3.13, 3.14 is abnormal: No module named 'backports'
-        python 01_hello_world.py
-        python 02_weather_control.py
-        python 03_spawn_traffic.py
-        python 04_sensor_capture.py
+        python 01_hello_world.py --port 3654
+        python 02_weather_control.py --port 3654
+        python 03_spawn_traffic.py --port 3654
+        python 04_sensor_capture.py --port 3654
         echo Finished Carla-Air Python API for Python.%%i example tests.
     )
 )
