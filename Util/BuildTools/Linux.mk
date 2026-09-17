33d6024fb00ece7e17f4c4164017498f78518e4f
xiao-ma-gua <17358869549@163.com>
2026-09-17 14:30:11 +0800

    加入冒烟、空中、VR、上传的check指令

--- Util/BuildTools/Linux.mk
+++ Util/BuildTools/Linux.mk
@@ -61,8 +61,21 @@ check.LibCarla.debug: LibCarla.debug
 check.LibCarla.release: LibCarla.release
 	@${CARLA_BUILD_TOOLS_FOLDER}/Check.sh --libcarla-release $(ARGS)
 
-check.PythonAPI: PythonAPI
+# check.PythonAPI: PythonAPI
+check.PythonAPI:
 	@${CARLA_BUILD_TOOLS_FOLDER}/Check.sh --python-api $(ARGS)
+
+check.smoke:
+	@${CARLA_BUILD_TOOLS_FOLDER}/Check.sh --smoke $(ARGS)
+
+check.air:
+	@${CARLA_BUILD_TOOLS_FOLDER}/Check.sh --air $(ARGS)
+
+check.vr:
+	@${CARLA_BUILD_TOOLS_FOLDER}/Check.sh --vr $(ARGS)
+
+check.upload:
+	@${CARLA_BUILD_TOOLS_FOLDER}/Check.sh --upload $(ARGS)
 
 check.PythonAPI.3: PythonAPI.3
 	@${CARLA_BUILD_TOOLS_FOLDER}/Check.sh --python-api --python-version=3 $(ARGS)
